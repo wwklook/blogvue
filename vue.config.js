892bce4310dedda25c6b30f1b9374805abff0ce8
wwklook <1367541215@qq.com>
2020-10-23 18:03:50 +0800

    blog

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const path = require('path');
 
 function resolve(dir) {
@@ -6,17 +5,6 @@ function resolve(dir) {
 }
 module.exports = {
     assetsDir: 'blog_static',
-    indexPath: 'blog.html'
-        //publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
-=======
-const path = require('path');
-
-function resolve(dir) {
-  return path.join(__dirname, dir)
-}
-module.exports = {
-  assetsDir: 'blog_static',
-  indexPath: 'blog.html',
-  //publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
->>>>>>> 10f06de9ccb2838bcb374c3ec5a29b0511949700
+    indexPath: 'blog.html',
+    //publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
 }
