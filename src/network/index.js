94d24b9f673ebd1bef58db7239e87e3cfcbe6daa
wwk <45363781+wwklook@users.noreply.github.com>
2023-04-01 17:22:00 +0800

    Update index.js

--- src/network/index.js
+++ src/network/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function requests(config) {
   const instance = axios.create({
-    baseURL: "https://www.wwklook.com/",
+    baseURL: "http://118.126.91.79/",
     // baseURL: "http://127.0.0.1:8000/",
     timeout: 10000,
   })
